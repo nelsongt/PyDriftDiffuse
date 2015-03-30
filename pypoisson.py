fix sign issues, constructor-like functions

pypoisson.py
@@ -9,6 +9,13 @@ from scipy import optimize
 from scipy.sparse import linalg
 
 
+## Constants ##
+q = 1 # e
+e_0 = 10.91 # e^2/chbar # vacuum permittivity
+k_B = 8.6173324E-05 # eV/K
+meter_eV_factor = 1.9733E-07 # m*eV/chbar
+
+
 ## Material Constants ##
 T = 300 # K
 e_r = 12.9 # GaAs relative permittivity
@@ -16,53 +23,72 @@ N_d = 1.0E+22 # 1/m^3 # doping in n-type
 N_a = 1.0E+22 # 1/m^3 # doping in p-type
 n_i = 2.498E+12 # 1/m^3
 
-## Constants ##
-q = 1 # e
-e_0 = 10.91 # e^2/chbar # vacuum permittivity
-k_B = 8.6173324E-05 # eV/K
-meter_eV_factor = 1.9733E-07 # m*eV/chbar
+p_type_width = 2E-06 # m
+n_type_width = 2E-06 # m
+
 
 ## Mode Settings ## 
-grid_pnts = 200 # must be even number for now
-cell_width = 4E-06 / meter_eV_factor
+grid_pnts = 200 # must be an even number for now
 
 
-## Material Calculations ##
-V_bi_p = (k_B*T/q)*math.asinh(-N_a/(2*n_i))
-V_bi_n = (k_B*T/q)*math.asinh(N_d/(2*n_i))
 
-N_d_eV = N_d * meter_eV_factor**3 # using units in relation to eV to keep values close to 1
-N_a_eV = N_a * meter_eV_factor**3 # could use any arbitrary units but this is good
-n_i_eV = n_i * meter_eV_factor**3
-
-del_x = cell_width/(grid_pnts - 1) # want 1 fewer 'gaps' than points
-del_x_2 = del_x ** 2
-
-
-## Construct constant solver matrix ##
+## Construct constant FDM matrix ##
 diag = np.zeros(grid_pnts-2) - 2
 udiag = np.zeros(grid_pnts-3) + 1
 ldiag = np.zeros(grid_pnts-3) + 1
 fdm_mat = sparse.diags([diag, udiag, ldiag], [0, 1, -1], shape=(grid_pnts-2, grid_pnts-2), format="csc")
 
 
-##Functions##
+## Build material functions ##
+def one_sided_Vbi(doping): # pass negative doping for p-type material
+  return (k_B*T/q)*math.asinh(doping/(2*n_i))
+
+def meter_conc_to_eV_conc(concentration):
+  return concentration * meter_eV_factor**3
+
+def meter_to_eV(distance):
+  return distance / meter_eV_factor
+
+def pnts_in_type(width,grid_distance):
+  return int(width / grid_distance) + 1
+
+## Material Calculations ##
+V_bi_p = one_sided_Vbi(-N_a)
+V_bi_n = one_sided_Vbi(N_d)
+
+N_d_eV = meter_conc_to_eV_conc(N_d) # using units in relation to eV to keep values close to 1
+N_a_eV = meter_conc_to_eV_conc(N_a) # could use any arbitrary units but this is good
+n_i_eV = meter_conc_to_eV_conc(n_i)
+
+cell_width_eV = meter_to_eV(n_type_width + p_type_width)
+
+del_x = cell_width_eV/(grid_pnts - 1) # want 1 fewer 'gaps' than points
+del_x_2 = del_x ** 2
+
+pnts_in_p = pnts_in_type(meter_to_eV(p_type_width),del_x)
+pnts_in_n = pnts_in_type(meter_to_eV(n_type_width),del_x)
+
+print pnts_in_n
+print pnts_in_p
+
+
+## The big function ##
 def big_func(Phi):
-  
+
   rhs = np.zeros(grid_pnts-2) # only interior grid points needed here
 
   # Take note of junction polarity and assign charge densities for each grid point
   halfway = int(rhs.size/2)
   for i in xrange(halfway):
-    n_eV = n_i_eV * math.exp(-q*Phi[i]/(k_B*T))
-    p_eV = n_i_eV * math.exp(q*Phi[i]/(k_B*T))
-    rho_n = q * (p_eV - n_eV + N_d_eV) # charge density in n-type
-    rhs[i] = del_x_2 * rho_n / (e_r*e_0) # setup rhs for n-type
+    n_eV = n_i_eV * math.exp(q*Phi[i]/(k_B*T))
+    p_eV = n_i_eV * math.exp(-q*Phi[i]/(k_B*T))
+    rho_p = -q * (p_eV - n_eV - N_a_eV) # charge density in p-type
+    rhs[i] = del_x_2 * rho_p / (e_r*e_0) # setup rhs for p-type
   for i in xrange(halfway):
-    n_eV = n_i_eV * math.exp(-q*Phi[i+halfway]/(k_B*T))
-    p_eV = n_i_eV * math.exp(q*Phi[i+halfway]/(k_B*T))
-    rho_p = q * (p_eV - n_eV - N_a_eV) # charge density in p-type
-    rhs[i+halfway] = del_x_2 * rho_p / (e_r*e_0) # setup rhs for p-type
+    n_eV = n_i_eV * math.exp(q*Phi[i+halfway]/(k_B*T))
+    p_eV = n_i_eV * math.exp(-q*Phi[i+halfway]/(k_B*T))
+    rho_n = -q * (p_eV - n_eV + N_d_eV) # charge density in n-type
+    rhs[i+halfway] = del_x_2 * rho_n / (e_r*e_0) # setup rhs for n-type
 
   # Incorporate boundary conditions
   rhs[0] = rhs[0] - V_bi_p # add potential at p-type depletion-neutral region iface 
@@ -71,7 +97,8 @@ def big_func(Phi):
   
   return fdm_mat * Phi - rhs
 
-
+## End Functions ##
+ 
  
 #### MAIN ####
 
@@ -85,7 +112,7 @@ for i in xrange(halfway):
 
 potentials = np.zeros(grid_pnts) # Create an array of potentials, one for each grid point
 
-potentials[1:potentials.size-1:1] = optimize.newton_krylov(big_func,phi_guess,verbose=1) # Trick because numpy can't append arrays without copying them
+potentials[1:potentials.size-1:1] = optimize.newton_krylov(big_func,phi_guess,verbose=1,iter=10) # Trick because numpy can't append arrays without copying them
 
 potentials[0] = V_bi_p # insert known boundary potentials
 potentials[potentials.size-1] = V_bi_n