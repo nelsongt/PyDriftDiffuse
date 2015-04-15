7253439437b5773d8512503b060324ed9971d284
George <deft@archlinux.us>
2015-04-15 17:32:53 -0400

    finished carrier concentration parts, debugging needed

--- pypoisson.py
+++ pypoisson.py
@@ -42,8 +42,15 @@ fdm_mat = sparse.diags([diag, udiag, ldiag], [0, 1, -1], shape=(grid_pnts-2, gri
 
 
 ## Build material functions ##
-def one_sided_Vbi(doping): # pass negative doping for p-type material
-  return (k_B*T/q)*math.asinh(doping/(2*n_i))
+def type_modifier(type): # 1 for p-type, 0 for n-type... I should make a typedef'ed enumerated list here
+  if type: # is true
+    return -1
+  else:
+    return 1
+
+def one_sided_Vbi(doping,type):
+  mod = type_modifier(type)
+  return (k_B*T/q)*math.asinh(mod*doping/(2*n_i))
 
 def meter_conc_to_eV_conc(concentration):
   return concentration * meter_eV_factor**3
@@ -62,8 +69,8 @@ def pnts_in_type(width,grid_distance):
 
 
 ## Material Calculations ##
-V_bi_p = one_sided_Vbi(-N_a)
-V_bi_n = one_sided_Vbi(N_d)
+V_bi_p = one_sided_Vbi(N_a,1)
+V_bi_n = one_sided_Vbi(N_d,0)
 
 N_a_eV = meter_conc_to_eV_conc(N_a) # using units in relation to eV to keep values close to 1
 N_d_eV = meter_conc_to_eV_conc(N_d) # could use any arbitrary units but this is good
@@ -89,6 +96,17 @@ def carrier_conc_from_phi(Phi): # pass negative phi for p-type material
   return n_i_eV * math.exp(q*Phi/(k_B*T))
   
 
+def ohmic_carrier_conc(type):  
+  mod = type_modifier(type)
+  if mod == -1:
+    doping = N_a_eV
+  elif mod == 1:
+    doping = N_d_eV
+  majority = sqrt(doping**2 + 4 * n_i_eV**2)/2 - mod*(doping)/2 
+  minority = n_i_eV**2 / majority
+  return (majority, minority)
+
+
 def Bernoulli(x):
   idxs = (np.abs(x) < 1e-12)
   idxs2 = ( np.abs(x) >= 1e-12)
@@ -97,43 +115,54 @@ def Bernoulli(x):
   B[idxs2] = x[idxs2] / np.expm1( x[idxs2] )
   return B
 
-def conc_factor(Diff,Phi1,Phi2):
-  return Diff * Bernoulli((Phi1 - Phi2)/(k_B*T))
 
-def carrier_conc_from_continuity(Phi,Diff)
+def conc_factor(Diff,Phi1,Phi2,type):
+  mod = type_modifier(type)
+  return Diff * Bernoulli(mod*(Phi1 - Phi2)/(k_B*T))
+
+
+def carrier_conc_from_continuity(Phi,Diff,type,polarity):  # Phi has total grid points, polarity is 1 for p-type at 0, 0 for n-type at 0 ... typedef?
   n_or_p = np.zeros(Phi.size)
-  
+
   # For now set Generation = 0 and Recombination = 0
   rhs = np.zeros(Phi.size-2)
   
   for i in xrange(1,Phi.size-1):
-    diag2[i] = -(conc_factor(Diff[i+1],Phi[i],Phi[i+1]) + conc_factor(Diff[i],Phi[i],Phi[i-1]))
+    diag2[i] = -(conc_factor(Diff[i+1],Phi[i],Phi[i+1],type) + conc_factor(Diff[i],Phi[i],Phi[i-1],type))
   for i in xrange(1,Phi.size-2):
-    udiag2[i] = conc_factor(Diff[i+1],Phi[i+1],Phi[i])
-    ldiag2[i] = conc_factor(Diff[i],Phi[i-1],Phi[i])
+    udiag2[i] = conc_factor(Diff[i+1],Phi[i+1],Phi[i],type)
+    ldiag2[i] = conc_factor(Diff[i],Phi[i-1],Phi[i],type)
   conc_mat = sparse.diags([diag2, udiag2, ldiag2], [0, 1, -1], shape=(Phi.size-2, Phi.size-2), format="csc")
   
   # Incorporate boundary conditions
-  n_or_p[0] = sqrt((N_d_eV - N_a_eV)**2 + 4 * n_i_eV**2)/2 + (N_d_eV - N_a_eV)/2 
-  n_or_p[n_or_p.size-1] = sqrt((N_d_eV - N_a_eV)**2 + 4 * n_i_eV**2)/2 + (N_d_eV - N_a_eV)/2
-  rhs[0] = conc_factor(Diff[i],Phi[i-1],Phi[i]) * n_or_p[0] 
-  rhs[rhs.size-1] = conc_factor(Diff[i+1],Phi[i+1],Phi[i]) * n_or_p[n_or_p.size-1]
+  if polarity: # is true (p-type at 0)
+    (p0,n0) = ohmic_carrier_conc(true)
+    (pF,nF) = ohmic_carrier_conc(false)
+  else: # n-type at 0
+    (n0,p0) = ohmic_carrier_conc(false)
+    (nF,pF) = ohmic_carrier_conc(true)
+  
+  n_or_p[0] = p0
+  n_or_p[n_or_p.size-1] = pF
+  rhs[0] = conc_factor(Diff[i],Phi[i-1],Phi[i],type) * n_or_p[0] 
+  rhs[rhs.size-1] = conc_factor(Diff[i+1],Phi[i+1],Phi[i],type) * n_or_p[n_or_p.size-1]
   
   
   # Solve for carrier concentrations using linear solver
   n_or_p[1:n_or_p.size-1:1] = sparse.linalg.spsolve(conc_mat,rhs)
   return n_or_p
 
-def big_func(Phi,n,p): # The big linear algebra setup function 
+
+def big_func(Phi,n,p): # The big linear algebra setup function, phi has (grid points - 2)
 
   rhs = np.zeros(phi.size) # only interior grid points needed here
 
   # Take note of junction polarity and assign charge densities for each grid point
   for i in xrange(rhs.size):
     if i < (pnts_in_p):
-      rho = -q * (p - n - N_a_eV) # charge density in p-type
+      rho = -q * (p[i] - n[i] - N_a_eV) # charge density in p-type
     else:
-      rho = -q * (p - n + N_d_eV) # charge density in n-type
+      rho = -q * (p[i] - n[i] + N_d_eV) # charge density in n-type
     rhs[i] = del_x_2 * rho / (e_r*e_0) # setup rhs for p-type
 
   # Incorporate boundary conditions
@@ -150,11 +179,18 @@ def big_func(Phi,n,p): # The big linear algebra setup function
 
 # Build guess vectors
 phi_guess = np.zeros(grid_pnts - 2)
+diffusivity = np.zeros(grid_pnts)
+n_conc = np.zeros(grid_pnts)
+p_conc = np.zeros(grid_pnts)
 for i in xrange(phi_guess.size):
   if i < (pnts_in_p-1):
     phi_guess[i] = V_bi_p # charge density in p-type
+    diffusivity[i+1] = D_p
   else:
     phi_guess[i] = V_bi_n # charge density in n-type
+    diffusivity[i+1] = D_n
+diffusivity[0] = D_p
+diffusivity[diffusivity.size-1] = D_n
 
 potentials = np.zeros(grid_pnts) # Create an array of potentials, one for each grid point
 
@@ -163,6 +199,9 @@ potentials[0] = V_bi_p # insert known boundary potentials
 potentials[potentials.size-1] = V_bi_n
 
 potentials[1:potentials.size-1:1] = phi_guess
+
+p_conc = carrier_conc_from_continuity(potentials,diffusivity,1,1)
+n_conc = carrier_conc_from_continuity(potentials,diffusivity,0,1)
 
 potentials[1:potentials.size-1:1] = optimize.newton_krylov(big_func,phi_guess,verbose=1,iter=10) # Trick because numpy can't append arrays without copying them
 
